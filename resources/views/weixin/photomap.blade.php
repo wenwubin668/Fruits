26d32fd1629ff496769bca5433d85a5c270316ff
fangjiabin <fangjiabin@ddweilai.com>
2018-12-08 18:12:14 +0800

    照片墙

--- resources/views/weixin/photomap.blade.php
+++ resources/views/weixin/photomap.blade.php
@@ -2,8 +2,8 @@
 <html lang="zh">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>纯js和CSS3分散式宝丽来图片画廊</title>
     <link rel="stylesheet" type="text/css" href="/marry/css/polaroid-gallery.css?v={{time()}}"/>
 </head>
