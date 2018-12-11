8195b318cf3c841493c14a613fbf6b53e0c7f04c
fangjiabin <fangjiabin@ddweilai.com>
2018-12-11 13:32:20 +0800

    信用卡

--- app/Console/Commands/Task/OutAmount.php
+++ app/Console/Commands/Task/OutAmount.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands\Task;
 
 use App\Common\CommonConf;
-use App\Console\Commands\TaskLog\TemplateMsgTraits;
 use App\Services\Card\CardService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
